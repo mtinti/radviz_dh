2aea4c9be3d93b5c593f7d2a17ed9c403c3f8bd3
mtinti <michele.tinti@gmail.com>
2020-07-07 22:07:42 +0100

    start

---
+++ index.js
@@ -0,0 +1,29 @@
+// dataset: iris
+d3.csv('./data/indata2.csv').then(
+	function(data) {
+	
+	
+	const IDtable = document.querySelector('#data-table');//the container of table
+	const IDradviz = document.querySelector('#radviz');//the container of radviz
+	const titles = d3.keys(data[0]);//titles in the data table
+	const colorAccessor = function(d){ return d['class']; };//dimension used for coloring
+	const dimensions = ['LG', 'G1', 'S', 'G2M', 'GG2M'];//dimensions used for RadViz.
+	const dimensionAnchor = Array.apply(null, 
+		{length: dimensions.length}).map(Number.call, Number).map(x=>x*2*Math.PI/(dimensions.length)); // intial DA configration;
+	
+	//console.log('index.js:titles', titles);
+	//console.log('index.js:colorAccessor', colorAccessor);
+	//console.log('index.js:dimensions', dimensions);
+	//console.log('index.js:dimensionAnchor', dimensionAnchor);
+	
+	// call the plot function
+	RadViz()
+		.DOMTable(IDtable)
+		.DOMRadViz(IDradviz)
+		.TableTitle(titles)
+		.ColorAccessor(colorAccessor)
+		.Dimensionality(dimensions)
+		.DAnchor(dimensionAnchor)
+		.DATA(data)
+		.call();
+}); 
